49804ea7cae0a3abab81228e047bde20ab347fa7
Filippo Gatti <filippo.gatti@centralesupelec.fr>
2023-01-11 18:42:55 +0100

    Choose HODMD to fast fit 1D time-histories
    Signed-off-by: Filippo Gatti <filippo.gatti@centralesupelec.fr>

--- notebooks/pydmd_analysis.py
+++ notebooks/pydmd_analysis.py
@@ -10,7 +10,8 @@ from matplotlib import pyplot as plt
 import re
 import pandas as pd
 import numpy as np
-from pydmd import SubspaceDMD
+from ezyrb import POD, RBF
+from pydmd import HODMD, ParametricDMD
 from scipy.signal import detrend
 from scipy.integrate import cumulative_trapezoid
 import pdb
@@ -35,134 +36,144 @@ parent_dir = os.path.abspath('..')
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
 
-# Path2RawData = os.path.join(os.path.abspath(""),r'..',r'data',r'rawtxt_data')
-
-# datalogger = lambda s: s.split("#")[1].split("__")[0]
-# datehour =  lambda s: s.split("__0_")[1].split(".filename")[0]
-# rawfile_list = [f for f in listdir(Path2RawData) if f.endswith('.filename')]
-# rawfile_list.sort(key=datehour)
-
-# Path2CsvData = os.path.join(os.path.abspath(""), r'..', r'data', r'csv_data')
-# csvfile_list = [f for f in listdir(Path2CsvData) if f.endswith('.csv')]
-# csvfile_list.sort(key=datehour)
-
-
 Path2ParquetData = os.path.join(os.path.abspath(""),
                                 r'..', r'data', r'parquet_data')
-
-# Date_Heure = len(rawfile_list)*[0]
-# for i, filename in enumerate(rawfile_list):
-#     date, heure = re.findall(r'(\d{4}-\d{2}-\d{2})_(\d{2}-\d{2}-\d{2})', filename)[0]
-#     Date_Heure[i] = f'{date} {heure}'
-
-fs = 100.0 # Hz - Sampling frequency
-fmin = 0.0  # Hz
-fmax = 2.0  # Hz
-scaling = 1.0
-r = 1
-
-
 
 df = dd.read_parquet(os.path.join(Path2ParquetData, "Datalogger.parquet"),
                      engine='pyarrow')
 df = df.set_index("Date_Heure", sorted=True)
 
-df = df.map_partitions(lambda df: df.assign(ü_r=detrend(df.Capteur_1), 
-                                            ü_θ=detrend(df.Capteur_2)), 
+df = df.map_partitions(lambda df: df.assign(a_r=detrend(df.Capteur_1), 
+                                            a_t=detrend(df.Capteur_2)), 
                        meta={"Capteur_1": 'f8', 
                              "Capteur_2": 'f8', 
                              "Temperature": 'f8', 
-                             "ü_r": 'f8', 
-                             "ü_θ": 'f8'}
+                             "a_r": 'f8', 
+                             "a_t": 'f8'}
                        ).drop("Capteur_1", axis=1).drop("Capteur_2", axis=1)
-df = df.map_partitions(lambda df: df.assign(ũ_r=cumulative_trapezoid(y=df.ü_r,
+df = df.map_partitions(lambda df: df.assign(v_r=cumulative_trapezoid(y=df.a_r,
                                                                      dx=0.01,
                                                                      initial=0),
-                                            ũ_θ=cumulative_trapezoid(y=df.ü_θ,
+                                            v_t=cumulative_trapezoid(y=df.a_t,
                                                                      dx=0.01,
                                                                      initial=0)),
                        meta={"Temperature": 'f8',
-                             "ü_r": 'f8',
-                             "ü_θ": 'f8',
-                             "ũ_r": 'f8',
-                             "ũ_θ": 'f8',}
+                             "a_r": 'f8',
+                             "a_t": 'f8',
+                             "v_r": 'f8',
+                             "v_t": 'f8',}
                        )
 
-# sub_dmd = DMD(svd_rank=-1, forward_backward=True)
-# dmd_r = MrDMD(sub_dmd,
-#               max_level=10,
-#               max_cycles=15)
-# dmd_θ = MrDMD(sub_dmd,
-#               max_level=10,
-#               max_cycles=15)
-dmd = SubspaceDMD(svd_rank=3, opt=True,)
 
 
-# vtm = df.loc[:"2022", :].index.compute()
-# vtm_dmd = vtm.to_numpy()[np.argwhere(np.logical_or(vtm.year == 2016, vtm.year == 2017))]
-dmd.fit(X=df.loc["2016-10-26", 
-                 ["ũ_r", 
-                  "ũ_θ",]
-                 ].sample(frac=0.1).compute().values.reshape(-1, 2).T)
-# dmd_θ.fit(X=df.loc["2016":"2017", "ũ_θ"].compute().values.reshape(1, -1))
+    
+# sub_dmd = SubspaceDMD(svd_rank=-1, opt=True,)
+# dmd = MrDMD(sub_dmd, max_cycles=10, max_level=10)
+# dmd = RDMD(oversampling=10,
+#            power_iters=2,
+#            svd_rank=-1,
+#            forward_backward=True,
+#            opt=True)
 
-# dmd_r.dmd_time['dt']=
-dmd.dmd_time['tend'] *= 3.0
+# params = df.Temperature.map_partitions(lambda x: x.mean()).compute().values[:4]
 
-pdb.set_trace()
+# dmds = [HODMD(svd_rank=0,
+#               forward_backward=True,
+#               exact=True,
+#               opt=True,
+#               d=1200)]
+# pdmd = ParametricDMD(dmds, 
+#                      POD(svd_rank=-1), 
+#                      RBF())
+
+
+
+fss = '1S'
+
+X_r = df.loc["2016-10-26 17:11:40":"2016-10-26 17:26:39", 
+             ["v_r"]
+            ].sort_values("Date_Heure").resample(fss).first().compute()
+X_t = df.loc["2016-10-26 17:11:40":"2016-10-26 17:26:39",
+             ["v_t"]
+            ].sort_values("Date_Heure").resample(fss).first().compute()
+
+dmd = HODMD(svd_rank=0,
+            forward_backward=True,
+            exact=True,
+            opt=True,
+            d=100)
+
+dmd.fit(X=X_r.values.reshape(-1, 1).T)
+
+
+dmd.dmd_time['tend'] *= 2 # int((24*60+15)*60*100/int(fss.split('S')[0]))
+
+dtm = (X_r.index.values[1] - X_r.index.values[0])/np.timedelta64(1, 's')
+vt0 = X_r.index.values[0]
+
+dmd_vtm = vt0 + (dmd.dmd_timesteps*dtm*1e9).astype('timedelta64[ns]')
+
+
+# plt.plot(X_r.values.reshape(-1, 2))
+# plt.plot(dmd.reconstructed_data.real.T)
+# 
+# plt.show()
+
+# 
+
+fig, ax = plt.subplots(1, 1,
+                       sharex=True,
+                       sharey=True,
+                       figsize=(6, 3.5))
+
+
+
+
+
+
 
 fig, ax = plt.subplots(3, 1,
                        sharex=True,
                        sharey=False,
                        figsize=(6, 3.5))
 
-average = "1s"
-df.loc["2016-10-26":"2016-10-27",["ũ_r"]].compute().plot(
-    ax=ax[0], color='black', legend=False, )
-df.loc["2016-10-26":"2016-10-27",["ũ_θ"]].compute().plot(
-    ax=ax[1], color='black', legend=False, )
+# average = "1s"
+df.loc["2016-10-26":"2016-10-27",["v_r"]].compute().plot(
+    ax=ax[0], color='black', legend=r"recorded", )
+df.loc["2016-10-26":"2016-10-27",["v_t"]].compute().plot(
+    ax=ax[1], color='black', legend=r"recorded", )
 df.loc["2016-10-26":"2016-10-27",["Temperature"]].compute().plot(
-    ax=ax[2], color='black', legend=False, )
+    ax=ax[2], color='black', legend=r"recorded", )
 
 
-# l01 = ax[0].plot(vtm, dmd_r.reconstructed_data.T.real,
-l01 = ax[0].plot(vtm, dmd_r.reconstructed_data.T.real,
+l01 = ax[0].plot(dmd_vtm, dmd.reconstructed_data.real[0,:],
                  color='red',
-                 linewidth=0.6,
-                 label=r'$\tilde{\dot{u}}_r(t)$')
-# l01 = ax[0].plot(vtm, dmd_r.partial_reconstructed_data(level=2).real.T,
-#                  color='red',
-#                  linewidth=0.6,
-#                  label=r'$\tilde{\dot{u}}_r(t)$')
+                 linewidth=2,
+                 label=r'DMD')
 
-l11 = ax[1].plot(vtm, dmd_θ.reconstructed_data.T.real,
-                 color='red',
-                 linewidth=0.6,
-                 label=r'$\tilde{\dot{u}}_\theta(t)$')
-# l11 = ax[1].plot(vtm, dmd_θ.partial_reconstructed_data(level=2).real.T,
+
+# l11 = ax[1].plot(dmd_vtm, dmd.reconstructed_data.real[1, :],
 #                  color='red',
-#                  linewidth=0.6,
-#                  label=r'$\tilde{\dot{u}}_\theta(t)$')
+#                  linewidth=2,
+#                  label=r'DMD')
 
 ax[0].axis('tight')
-ax[0].set(xlim=(0.0, 900.0),
-          xlabel=r'$t$ [s]',
+ax[0].set(xlabel=r'$t$ [s]',
           ylabel=r'$\dot{u}_r(t)$ [m/s]',
-          title='Datalogger recorded on {} at {}'.format(date, heure))
+          title='Datalogger')
 ax[1].axis('tight')
-ax[1].set(xlim=(0.0, 900.0),
-          xlabel=r'$t$ [s]',
+ax[1].set(xlabel=r'$t$ [s]',
           ylabel=r'$\dot{u}_\theta(t)$ [m/s]')
-#   title='Datalogger recorded on {} at {}'.format(date,heure))
 
 ax[0].legend(frameon=False)
 ax[1].legend(frameon=False)
 
-fig.savefig("dmd.png",
-            format="png",
-            bbox_inches='tight',
-            dpi=300)
-plt.close()
+
+# fig.savefig("dmd.png",
+#             format="png",
+#             bbox_inches='tight',
+#             dpi=300)
+plt.show()
 
 # Ajout des valeurs de fréquence au fichier
 
@@ -193,7 +204,7 @@ plt.close()
 #         dt = t[1]-t[0]
         
 #         ddu_r_f = lambda s: np.interp(s, vtm, df_jour["Capteur_1"].values)
-#         ddu_θ_f = lambda s: np.interp(s, vtm, df_jour["Capteur_2"].values)
+#         ddu_t_f = lambda s: np.interp(s, vtm, df_jour["Capteur_2"].values)
 
 #         du_r = lambda s: np.interp(s, vtm,
 #                                    ict(y=ddu_r_f(vtm), 
@@ -201,14 +212,14 @@ plt.close()
 #                                        dx=dtm, 
 #                                        initial=0)
 #                                    )
-#         du_θ = lambda s: np.interp(s, vtm,
-#                                    ict(y=ddu_θ_f(vtm), 
+#         du_t = lambda s: np.interp(s, vtm,
+#                                    ict(y=ddu_t_f(vtm), 
 #                                        x=vtm, 
 #                                        dx=dtm, 
 #                                        initial=0)
 #                                    )
 #         X_r = np.nan_to_num(np.array(du_r(t), dtype=np.float64).flatten())
-#         X_θ = np.nan_to_num(np.array(du_θ(t), dtype=np.float64).flatten())
+#         X_t = np.nan_to_num(np.array(du_t(t), dtype=np.float64).flatten())
 
 #         # sub_dmd = SubspaceDMD(svd_rank=-1, opt=False,
 #         #                       rescale_mode=None, sorted_eigs=False)
