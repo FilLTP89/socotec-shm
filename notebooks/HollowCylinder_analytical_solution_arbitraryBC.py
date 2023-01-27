40f38f8217d63f89ff087b04b4657761031916f5
Filippo Gatti <filippo.gatti@centralesupelec.fr>
2023-01-27 19:07:36 +0100

    [BUGFIX] create symbolic equation 6a out of arrays and matrices
    Signed-off-by: Filippo Gatti <filippo.gatti@centralesupelec.fr>

--- notebooks/HollowCylinder_analytical_solution_arbitraryBC.py
+++ notebooks/HollowCylinder_analytical_solution_arbitraryBC.py
@@ -4,12 +4,13 @@ from sympy.plotting import plot
 from sympy import besselj as Jb
 from sympy import bessely as Yb
 from sympy import pi
-from sympy import Array, Matrix
+from sympy import Array, Matrix, Sum, MatrixSymbol
 # from sympy import symbols
 from sympy.abc import R, h, L, x, n, m, N, M
 from sympy.abc import E
 from sympy import Function, sin, cos
 from sympy.solvers.solveset import linsolve
+
 
 import pdb
 """_summary_
@@ -18,34 +19,38 @@ import pdb
      with Arbitrary Boundary Conditions. Applied Acoustics 2013, 74 (3), 440-449. 
      https://doi.org/10.1016/j.apacoust.2012.09.001.
 """
+# Reference system
+Ref = CoordSys3D('Oxyz', 
+                 vector_names=("ix", "iy", "iz")
+                )
+C = Ref.create_new('Orθz',
+                    transformation='cylindrical',
+                    variable_names=("r", "θ", "z"),
+                    vector_names=("ir", "iθ", "iz")
+                  )
 
+ur = Function("ur")(C.r, C.θ, C.z)
+uθ = Function("uθ")(C.r, C.θ, C.z)
+uz = Function("uz")(C.r, C.θ, C.z)
 
-(π, ν, ω, σ, λm, γ) = sp.symbols('π ν ω σ λm γ')
-N = 10
-M = 10
+N = 12  # number of circumferential modes
+M = 10  # number of longitudinal modes
 
-# (an, bn, cn, dn, en, fn, gn, hn) = sp.symbols('an bn cn dn en fn gn hn')
-# (α1, α2, β1, β2, β3, β4) = sp.symbols('α1 α2 β1 β2 β3 β4')
-
+# general symbols and variables
+(π, ν, ω, σ, γ) = sp.symbols('π ν ω σ γ', positive=True)
 π = pi
-λm = π*m/L # wavelength of the m-th tuned harmonic
+# λm = π*m/L # wavelength of the m-th tuned harmonic
+λ_s = sp.symbols("λ_0:{:d}".format(M+1))
+λm = Matrix(M+1, 1, λ_s)
+
+λm2 = sp.diag(*λ_s)*λm
 K = E*h/(1.0-σ**2) # in-plane stifness
 D = E*h**3/12.0/(1.0-σ**2) # flexural stiffness
 γ = h**2/12.0/R**2
 
 
-R = CoordSys3D('Oxyz', 
-               vector_names=("ix", "iy", "iz")
-               )
-C = R.create_new('Orθz',
-                 transformation='cylindrical',
-                 variable_names=("r", "θ", "z"),
-                 vector_names=("ir", "iθ", "iz")
-                 )
-
-ur = Function("ur")(C.r, C.θ, C.z)
-uθ = Function("uθ")(C.r, C.θ, C.z)
-uz = Function("uz")(C.r, C.θ, C.z)
+# Boundary conditions stiffnesses
+(k1, k2, k3, k4, k5, k6, k7, k8) = sp.symbols('k1 k2 k3 k4 k5 k6 k7 k8')
 
 # α and β vectors
 α1 = x*(x/L-1)**2
@@ -59,26 +64,43 @@ uz = Function("uz")(C.r, C.θ, C.z)
 β = Array([β1, β2, β3, β4])
 # Fourier's expansion coefficients
 
+a_s = sp.symbols("a_0:{:d}".format(N+1))
+b_s = sp.symbols("b_0:{:d}".format(N+1))
+c_s = sp.symbols("c_0:{:d}".format(N+1))
+d_s = sp.symbols("d_0:{:d}".format(N+1))
+e_s = sp.symbols("e_0:{:d}".format(N+1))
+f_s = sp.symbols("f_0:{:d}".format(N+1))
+g_s = sp.symbols("g_0:{:d}".format(N+1))
+h_s = sp.symbols("h_0:{:d}".format(N+1))
+an = Matrix(N+1, 1, a_s)
+bn = Matrix(N+1, 1, b_s)
+cn = Matrix(N+1, 1, c_s)
+dn = Matrix(N+1, 1, d_s)
+en = Matrix(N+1, 1, e_s)
+fn = Matrix(N+1, 1, f_s)
+gn = Matrix(N+1, 1, g_s)
+hn = Matrix(N+1, 1, h_s)
 
-av = sp.symbols("a_0:{:d}".format(N+1))
-bv = sp.symbols("b_0:{:d}".format(N+1))
-cv = sp.symbols("c_0:{:d}".format(N+1))
-dv = sp.symbols("d_0:{:d}".format(N+1))
-ev = sp.symbols("e_0:{:d}".format(N+1))
-fv = sp.symbols("f_0:{:d}".format(N+1))
-gv = sp.symbols("g_0:{:d}".format(N+1))
-hv = sp.symbols("h_0:{:d}".format(N+1))
-abcdefgh = Array([av, bv, cv, dv, ev, fv, gv, hv], (8*len(av),))
-
-Av = sp.symbols("A_0:{:d}".format((M+1)*(N+1)))
-Bv = sp.symbols("B_0:{:d}".format((M+1)*(N+1)))
-Cv = sp.symbols("C_0:{:d}".format((M+1)*(N+1)))
-
-ABC = Array([Av, Bv, Cv], (3*len(Av),))
-
+# an = ArraySymbol("a", (n,))
+Amn = Matrix(M+1, N+1, sp.symbols('A_0:{:d}(0:{:d})'.format(M+1, N+1)))
+Bmn = Matrix(M+1, N+1, sp.symbols('B_0:{:d}(0:{:d})'.format(M+1, N+1)))
+Cmn = Matrix(M+1, N+1, sp.symbols('C_0:{:d}(0:{:d})'.format(M+1, N+1)))
+ΣAn = sp.ones(1,Amn.shape[0])*Amn
+ΣBn = sp.ones(1,Bmn.shape[0])*Bmn
+ΣCn = sp.ones(1,Cmn.shape[0])*Cmn
+eq6a = an-\
+     (7.0*σ*L/(3.0*π*R)+3.0*π*R*γ/(4.0*L))*fn-\
+     (4.0*σ*L**3/(3.0*π**3*R)+L*γ*R/π)*hn-\
+    -((k1/K)*ΣAn-(σ*n/R)*ΣBn-(σ/R)*ΣCn-γ*R*λm2.T*Cmn).T
 pdb.set_trace()
-# linsolve([x + y + z - 1, 
-#           x + y + 2*z - 3], (x, y, z))
+
+
+    #Σ(Av[m], (m, 0, M))
+# lhs = Array([],
+#             (,))
+# abcdefgh = Array([av, bv, cv, dv, ev, fv, gv, hv], (8*len(av),))
+# ABC = Array([Av, Bv, Cv], (3*len(Av),))
+
 
 
 u = C.ir*ur+C.iθ*uθ+C.iz*uz
