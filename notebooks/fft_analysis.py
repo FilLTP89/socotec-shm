61556e27be0d45bef2ba310b8ba9d4ab179e2ef6
Filippo Gatti <filippo.gatti@centralesupelec.fr>
2023-02-21 13:33:32 +0100

    Test multiprocess scheduler

--- notebooks/fft_analysis.py
+++ notebooks/fft_analysis.py
@@ -5,16 +5,18 @@ from fast_konno_ohmachi import fast_konno_ohmachi as fko
 import os
 import sys
 import math
+import dask
 import dask.dataframe as dd
 # import dask.bag as db
 import dask.array as da
 import dask.array.fft as dfft
-from dask.distributed import Client
+from dask.distributed import LocalCluster, Client
 from matplotlib import pyplot as plt
 import pdb
 import numpy as np
 from scipy.signal import detrend, find_peaks
 from scipy.integrate import cumulative_trapezoid
+from concurrent.futures import ThreadPoolExecutor
 
 u"""General informations"""
 __author__ = "Filippo Gatti"
@@ -32,7 +34,11 @@ plt.rcParams.update({
     "font.family": "Helvetica"
 })
 
-os.environ['DASK_SCHEDULER_ADDRESS'] = 'tcp://localhost:8786'
+# os.environ['DASK_SCHEDULER_ADDRESS'] = 'tcp://localhost:8786'
+# '127.0.0.1:8786', threads_per_worker=1, n_workers=8)
+# cluster = LocalCluster()
+# client = Client(cluster)
+
 # Client()
 parent_dir = os.path.abspath('..')
 if parent_dir not in sys.path:
@@ -70,7 +76,7 @@ df = df.map_partitions(lambda df: df.assign(v_r=cumulative_trapezoid(y=df.a_r,
                              "v_t": 'f8',}
                        )
 
-fss = '0.25S'
+fss = '0.15S'
 dtm = float(fss.strip("S"))
 # rec = 15*60*np.timedelta64(fss.strip('S'), 's').astype(np.int64)
 df = df.resample(fss).first().dropna()
@@ -79,18 +85,10 @@ chunk_sizes = list(df.map_partitions(len).compute().values)
 
 ar = df[["v_r", "v_t", "Temperature"]].to_dask_array(lengths=chunk_sizes)
 
-nfq = np.stack([next_power_of_2(b.size) 
-                for b in ar[:, 2].blocks])
-vfq = [np.linspace(0.0,
-                   0.5/dtm,
-                   n//2+1,
-                   endpoint=True)
-       for n in nfq]
-
 FSA = ar.map_blocks(lambda ths: np.hstack([np.linspace(0.0,
-                                                      0.5/dtm,
-                                                      next_power_of_2(
-                                                          ths.shape[0])//2+1,
+                                                       0.5/dtm,
+                                                       next_power_of_2(
+                                                           ths.shape[0])//2+1,
                                                        endpoint=True).reshape(-1, 1),
                                            np.fft.rfft(ths,
                                                        n=next_power_of_2(ths.shape[0]),
@@ -104,17 +102,15 @@ FSA = ar.map_blocks(lambda ths: np.hstack([np.linspace(0.0,
                                   dtype=np.complex128),
                     )
 
-TFS = FSA.map_blocks(lambda fsa: np.hstack([fsa[:,0],
+TFS = FSA.map_blocks(lambda fsa: np.hstack([(fsa[1:, 0]+fsa[1, 0]).astype(np.float64).reshape(-1, 1),
                                             fko(np.abs(fsa[1:, 1].T/fsa[1:, 3].T),
-                                                np.linspace(0.0, 0.5/dtm, 
-                                                            fsa[:, 0].size,
-                                                            endpoint=True)[1:],
-                                                smooth_coeff=40), 
+                                                fsa[1:, 0].astype(np.float64),
+                                                smooth_coeff=40).reshape(-1, 1), 
                                             fko(np.abs(fsa[1:, 2].T/fsa[1:, 3].T), 
-                                                np.linspace(0.0, 0.5/dtm, 
-                                                           fsa[:,0].size, 
-                                                           endpoint=True)[1:], 
-                                                smooth_coeff=40)]),
+                                                fsa[1:, 0].astype(np.float64),
+                                                smooth_coeff=40).reshape(-1, 1)
+                                            ]
+                                           ),
                      dtype=np.float64,
                      enforce_ndim=True,
                      chunks=(tuple([fs.shape[0]-1 for fs in FSA.blocks]),
@@ -122,39 +118,65 @@ TFS = FSA.map_blocks(lambda fsa: np.hstack([fsa[:,0],
                              ),
                      meta=np.array((),
                                    dtype=np.float64),
-                     )
+                    )
 
 def get_TFS_peaks(tfs):
     min_prominence = 1000
-    p_r, pd_r = find_peaks(
-        tfs[:, 1], prominence=(None, 
-                         min_prominence), 
-        distance=100)
-    p_t, pd_t = find_peaks(
-        tfs[:, 2], prominence=(None,
-                               min_prominence),
-        distance=100)
-    pm_r = np.zeros((10,), 
-                    dtype=np.int64)
-    pm_t = np.zeros((10,), 
-                    dtype=np.int64)
-    pm_r[:pd_r['prominences'].size] = p_r[np.argsort(pd_r['prominences'])]
-    pm_t[:pd_t['prominences'].size] = p_t[np.argsort(pd_t['prominences'])]
-    return np.array([pm_r, pm_t]).T
+    
+    tfs = tfs[np.where(tfs[:, 0] >= 0.1) and np.where(tfs[:, 0] <= 3.0)]
+    p_r, pd_r = find_peaks(tfs[:, 1], 
+                           prominence=(None,
+                                       min_prominence),
+                           distance=100
+                           )
+    p_t, pd_t = find_peaks(tfs[:, 2], 
+                           prominence=(None,
+                                       min_prominence),
+                           distance=100
+                           )
+    ir = min(3, pd_r['prominences'].size)
+    it = min(3, pd_t['prominences'].size)
+    pm_r = np.zeros((ir,), dtype=np.int64)
+    pm_t = np.zeros((it,), dtype=np.int64)
+    pm_r = p_r[np.argsort(pd_r['prominences'])][:ir]
+    pm_t = p_t[np.argsort(pd_t['prominences'])][:it]
+    fr = tfs[np.sort(pm_r), 0]
+    ft = tfs[np.sort(pm_t), 0]
+    
+    return np.vstack([fr, ft]).T
 
 
 pks = TFS.map_blocks(get_TFS_peaks,
                      dtype=np.float64,
                      enforce_ndim=True,
-                     chunks=(tuple([10 for fs in TFS.blocks]),
+                     chunks=(tuple([3 for fs in TFS.blocks]),
                              (2,)
                              ),
                      meta=np.array((),
                                    dtype=np.float64),
                     )
 
-pdb.set_trace()
+Tavg = ar.map_blocks(lambda b: b[:,-1].mean(axis=0).reshape(-1,1),
+                     dtype=np.float64,
+                     enforce_ndim=True,
+                     chunks=(tuple([1 for fs in ar.blocks]), (1,)),
+                     meta=np.array((),
+                                   dtype=np.float64),
+                     )
 
+fig, ax = plt.subplots(1, 1,
+                       sharex=True,
+                       sharey=False,
+                       figsize=(6, 3.5))
+with dask.config.set(pool=ThreadPoolExecutor(2), num_workers=2, scheduler='processes'):
+    
+    ax.scatter(Tavg, pks[0::3], color='black', label="1")
+    ax.scatter(Tavg, pks[1::3], color='red', label="2")
+    ax.scatter(Tavg, pks[2::3], color='blue', label="3")
+
+# ax.scatter(Tavg, pks[3::5], color='pink', label="4")
+# ax.scatter(Tavg, pks[4::5], color='gray', label="5")
+plt.show()
 # plt.loglog(vfq[0], 
 #            np.abs(FSA.blocks[0][:, 0]/FSA.blocks[0][:, 2]), 
 #            color='black', 
